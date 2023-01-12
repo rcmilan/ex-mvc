56f91b3ef3b7079dc8cb75175d752484ccd53c6d
rcmilan <13418519+rcmilan@users.noreply.github.com>
2023-01-12 09:12:39 -0300

    Add project files.

---
+++ MVCApp/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public void Action1()
+        {
+            var x = "aaaaaaa";
+
+            x = "bbb";
+        }
+
+        public void Action2(string parameter123)
+        {
+            parameter123 = "bbb";
+        }
+    }
+}
