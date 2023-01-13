4c19ab68373c33bfd0c9000f45bac77fd05cf2eb
rcmilan <13418519+rcmilan@users.noreply.github.com>
2023-01-13 10:03:11 -0300

    enviando POST para controller

---
+++ MVCApp/Controllers/FormController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCApp.Models.Form;
+
+namespace MVCApp.Controllers
+{
+    public class FormController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult FormSubmit(FormModel form)
+        {
+            return View("Index");
+        }
+    }
+}
---
+++ MVCApp/Models/Form/FormModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCApp.Models.Form
+{
+    public class FormModel
+    {
+        [Display(Name = "Nome")]
+        public string Name { get; set; } = default!;
+
+        [Display(Name = "Numero Qualquer")]
+        public int UserNumber { get; set; } = default!;
+
+        [Display(Name = "Data Datada")]
+        public DateTime Date { get; set; } = default!;
+    }
+}
